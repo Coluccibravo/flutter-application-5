1b46234aa1e776dd8f4805be26dfcb1d150b5c81
Coluccibravo <cl203231@g.unicamp.br>
2025-03-10 10:45:32 -0300

    legal

--- lib/skyline.dart
+++ lib/skyline.dart
@@ -8,18 +8,34 @@ class Skyline extends StatefulWidget {
 }
 
 class _SkylineState extends State<Skyline> {
-  Image skylinef = Image.asset("images/jakob-rosen-qttIqS6II34-unsplash.jpg");
-  Image skylinet = Image.asset("images/patrik-storm-alstra-pictures-RBoUtBzuA8w-unsplash.jpg");
+  Image skylinef = Image.asset("images/jakob-rosen-qttIqS6II34-unsplash.jpg" , height: 500,width: 500);
+  Image skylinet = Image.asset("images/patrik-storm-alstra-pictures-RBoUtBzuA8w-unsplash.jpg", height: 500,width: 500);
   late Image Skylinea = skylinef;
   String textskylinef = "Skyline frente";
   String textskylinet = "Skyline atrás";
   late String textskylinea = textskylinef;
+
+  void mudaskyline(){
+    if(Skylinea == skylinef){
+      Skylinea = skylinet;
+      textskylinea = textskylinet;
+    }
+    else{
+      Skylinea = skylinef;
+      textskylinea == textskylinef;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: Column(children: [
       Skylinea,
       Text(textskylinea),
-
+      ElevatedButton(onPressed: () {
+        mudaskyline();
+        setState(() {
+        });
+      }, child: Text("Frente e trás"))
     ])));
   }
 }
